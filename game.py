7885b4e22a78e709ffcfbc8f1e03a4af0b2a83de
Mohsin Rizvi <mkrizvi98@gmail.com>
2017-04-27 18:53:53 -0400

    Add files via upload

---
+++ game.py
@@ -0,0 +1,20 @@
+"""
+
+game.py
+Contains the definition of the Game class, used to run the whole game.
+Written by:  Mohsin Rizvi
+Last edited: 04/27/17
+
+"""
+
+import npc
+import player
+
+# The Game object.
+class Game:
+
+	# Purpose:    Starts the game.
+	# Parameters: None
+	# Return:     Void
+	def __init__(self):
+		pass
