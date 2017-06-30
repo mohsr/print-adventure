6925748e975922538e2f109dfc55b5f88a626d28
Mohsin Rizvi <mkrizvi98@gmail.com>
2017-06-30 10:55:47 -0400

    separated npc and enemy files

--- npc.py
+++ npc.py
@@ -8,31 +8,12 @@ Last edited: 05/27/17
 """
 
 import bio
-import cbt_ability
 
 # A basic NPC to interact with.
 class NPC:
 
     # Purpose:    Construct a random NPC.
-    # Parameters: A string charType. It can be one of several strings to
-    #             specify a certain type of NPC to construct. If it is not
-    #             one of those, construct a random NPC. A string charRace
-    #             is also given.
+    # Parameters: A string NPC name.
     # Return:     Void
-    def __init__(self, charType, charRace):
-        if charType.lower() == "mob":
-            self.genMob(charRace)
-        elif charType.lower() == "vendor":
-            self.genVendor(charRace)
-
-    # Purpose:    Make the NPC a mob (generic enemy).
-    # Parameters: A string charRace indicating the race of the mob.
-    # Return:     Void
-    def genMob(self, charRace):
-        pass
-
-    # Purpose:    Make the NPC a vendor.
-    # Parameters: A string charRace indicating the race of the vendor.
-    # Return:     Void
-    def genVendor(self, charRace):
-        pass
+    def __init__(self, name):
+        self.name = name
