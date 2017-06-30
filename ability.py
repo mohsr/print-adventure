65ac6c47f775eb23e5d196e72fa0df92a52a1ca6
Mohsin Rizvi <mkrizvi98@gmail.com>
2017-06-30 11:04:58 -0400

    now used for combat abilities
    out of combat abilities will be replaced by out of combat stats (charisma, luck, perception)

--- ability.py
+++ ability.py
@@ -3,13 +3,13 @@
 ability.py
 Contains the definition of the Ability class.
 Written by:  Mohsin Rizvi
-Last edited: 05/27/17
+Last edited: 06/29/17
 
 """
 
 import basics
 
-# An ability to use out of combat.
+# An ability to use in-combat.
 class Ability:
 
     # Purpose:    Construct an ability.
